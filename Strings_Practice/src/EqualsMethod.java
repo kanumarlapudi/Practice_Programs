f73d0075624df3ebee3e7d84e69543dcb3f34a57
kanumarlapudi <180020034ce@gmail.com>
2022-12-01 20:04:55 +0530

    First commit

--- Strings_Practice/src/EqualsMethod.java
+++ Strings_Practice/src/EqualsMethod.java
@@ -6,18 +6,18 @@ public class EqualsMethod {
 		String s = "Sachin";
 		String s1 = new String("Sachin");
 		
-		System.out.println(s==s1);       // == compares the object
+		System.out.println(s==s1);       // == compares the object reference
 		System.out.println(s.equals(s1));    // equals method compares the content
 		
 		
 		StringBuilder s2 = new StringBuilder("Dhoni ");
 		StringBuilder s3 = new StringBuilder("Dhoni ");
-		System.out.println(s2==s3);      // == compares the object
+		System.out.println(s2==s3);      // == compares the object reference
 		
 		
 		StringBuilder s4 = new StringBuilder("Dhoni ");
 		StringBuilder s5 = new StringBuilder("Dhoni ");
-		System.out.println(s4.equals(s5));      // == compares adress of the object not content
+		System.out.println(s4.equals(s5));      // == compares address of the object not content
 		
        // StringBuilder methods all are same in stringBuffer 
 		
