366a099dff2bd104cd92a12de69d1979644e04c0
kanumarlapudi <180020034ce@gmail.com>
2022-12-22 20:55:03 +0530

    First Commit

---
+++ HAS_A_Relationship_PrimitiveValueInjection/src/Harish/main/test.java
@@ -0,0 +1,18 @@
+package Harish.main;
+
+//  For this writing the package name must be same thats it we can achieve relationship between 2 classes
+
+public class test {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+		Student st = new Student(34,180020034,"Harish");
+		Student st1 = new Student(4,180020004,"Suhas");
+		
+		System.out.println(st);
+		System.out.println(st1);
+		
+	}
+
+}
